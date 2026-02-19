491d5579c8c7ccc483929bef4adbabff2dd11ec3
Retyer7779-speedruner-P2 <gasfaoisurf@gmail.com>
2026-02-19 18:58:22 +0300

    Project #6

--- mine.py
+++ mine.py
@@ -8,7 +8,7 @@ def main():
 
 @app.route("/index")
 def index():
-    return "И на Марсе будут яблони цвести!"
+    return render_template("base.html", title="Заготовка")
 
 @app.route("/promotion")
 def promotion():
@@ -28,7 +28,7 @@ def promotion_image():
 
 @app.route("/astronaut_selection")
 def astronaut_selection():
-    return render_template("astronaut_selection.html")
+    return render_template("astronaut_selection.html", title="Отбор астронавтов")
 
 if __name__ == "__main__":
     app.run("127.0.0.1", 8080)
